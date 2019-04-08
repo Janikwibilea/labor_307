90e435433d1d01d46ec1e1b867cc47dba5eda4e3
Wehrli Janik <janik.wehrli@blnet.lokal>
2019-04-08 11:29:28 +0200

    d

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+$(".bilderunten1").on("click" ,function(){
+$("#bilddiashowgross").attr("src", "img/2000px-PHP-logo.svg.png");
+});
+$(".bilderunten2").on("click" ,function(){
+    $("#bilddiashowgross").attr("src", "img/xammp.jpg");
+    });
+    $(".bilderunten3").on("click" ,function(){
+        $("#bilddiashowgross").attr("src", "img/Visual_Studio_2013_Logo.svg.png");
+        });
+
+
+
+
+});
